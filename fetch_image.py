dc5b93520a2d5d2cfc8ee08f8fbde5765ed3793b
SOK Piseth <2023467sok@aupp.edu.kh>
2024-12-19 21:06:14 +0700

    Create fetch_image.py

---
+++ fetch_image.py
@@ -0,0 +1,16 @@
+import requests
+import os
+
+response = requests.get("https://meme-api.com/gimme")
+data = response.json()
+
+image_url = data['url']
+title = "meme_of_the_day"
+image_extension = image_url.split('.')[-1]
+
+image_response = requests.get(image_url)
+os.makedirs('memes', exist_ok=True)
+image_path = f"memes/{title}.{image_extension}"
+
+with open(image_path, 'wb') as f:
+    f.write(image_response.content)
