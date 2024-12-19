2e5336ecb9ff015c97bffc8e93334386c78598af
SOK Piseth <2023467sok@aupp.edu.kh>
2024-12-19 21:45:00 +0700

    Update fetch_image.py

--- fetch_image.py
+++ fetch_image.py
@@ -1,16 +1,18 @@
 import requests
 import os
 
-response = requests.get("https://meme-api.com/gimme")
-data = response.json()
+image_extension=""
+while(image_extension!="png"):
+    response = requests.get("https://meme-api.com/gimme")
+    data = response.json()
+    image_url = data['url']
+    image_extension = image_url.split('.')[-1]
 
-image_url = data['url']
-title = "meme_of_the_day"
-image_extension = image_url.split('.')[-1]
-
+title = "meme_of_the_day"    
 image_response = requests.get(image_url)
 os.makedirs('memes', exist_ok=True)
 image_path = f"memes/{title}.{image_extension}"
 
 with open(image_path, 'wb') as f:
     f.write(image_response.content)
+
